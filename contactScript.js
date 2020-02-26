4a7041c161e777b6ee63faf06f7a64d07b1ed687
Jessica <duellje@oregonstate.edu>
2020-02-26 16:37:52 -0500

    changed style on form in contact page

--- contactScript.js
+++ contactScript.js
@@ -8,7 +8,7 @@ function bindButton(){
     var email = document.getElementByName('email');
     var comment = document.getElementByName('comment');
     var completeForm = firstName + ' ' + lastName + ' ' + email + ' ' + comment;
-    req.open('POST', ' http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', false);
+    req.open('GET', ' http://web.engr.oregonstate.edu/~zhangluy/tools/class-content/form_tests/check_request.php', false);
     req.setRequestHeader('Content-Type', 'application/json');
     req.send(JSON.stringify(completeForm));
     event.preventDefault();
