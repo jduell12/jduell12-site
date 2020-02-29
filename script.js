66244204c0585e1fd184c22ccb6d8b61decdaf14
Jessica <duellje@oregonstate.edu>
2020-02-28 19:18:08 -0500

    tweaked carosaul

--- script.js
+++ script.js
@@ -1,31 +1,35 @@
-var slideIndex = 1;
 
-showSlides(slideIndex);
+var slideIndex = 0;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+
+
+function currentSlide(n){
+  slideIndex = n;
+  showSlides(slideIndex);
+}
 
 function nextSlide(n){
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n){
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n){
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+
   if(n > slides.length){
     slideIndex = 1;
   }
   if(n < 1){
     slideIndex = slides.length;
   }
+
   for(i = 0; i < slides.length; i++){
     slides[i].style.display = "none";
   }
   for(i = 0; i < dots.length; i++){
     dots[i].className = dots[i].className.replace(" active", "");
   }
+
   slides[slideIndex-1].style.display = "block";
-  dot[slideIndex-1].className += "active";
+  dots[slideIndex-1].className += " active";
 }
